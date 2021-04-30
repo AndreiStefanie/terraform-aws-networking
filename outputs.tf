da1d66bb8065e3bb9d75c01ac55085365f41d9f3
Andrei Stefanie <andrei.stefanie@gmail.com>
2021-04-30 12:11:59 +0300

    Added outputs

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.mtc_vpc.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "public_sg" {
+  value = aws_security_group.this["public"].id
+}
+
+output "db_subnet_group_name" {
+  value = aws_db_subnet_group.rds[*].name
+}
+
+output "db_security_group" {
+  value = aws_security_group.this["rds"].id
+}
